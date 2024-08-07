0e60f5a569148aeb43a98272248255e49cad3e05
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-08-07 10:32:01 +0100

    router updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -124,7 +124,7 @@ const Home = () => {
                 </p>
 
                 <Link
-                  to="/doctors"
+                  to="/login"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                   <BsArrowRight className="group-hover:text-white w-6 h-5" />
@@ -148,7 +148,7 @@ const Home = () => {
                 </p>
 
                 <Link
-                  to="/doctors"
+                  to="/login"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                   <BsArrowRight className="group-hover:text-white w-6 h-5" />
@@ -172,7 +172,7 @@ const Home = () => {
                 </p>
 
                 <Link
-                  to="/doctors"
+                  to="/login"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                   <BsArrowRight className="group-hover:text-white w-6 h-5" />
