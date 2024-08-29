d45716d21aaa1e31bcfb53065966609c49491bac
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-08-29 22:00:53 +0100

    review

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import starIcon from "../../assets/images/Star.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
+import Profile from "../../assets/images/profile.png";
 
 const DoctorCard = ({ doctor }) => {
   const {
@@ -18,23 +19,27 @@ const DoctorCard = ({ doctor }) => {
   return (
     <div className="p-3 lg:p-5">
       <div>
-        <img src={photo} className="w-full object-cover rounded" alt={name} />
+        <img
+          src={photo || Profile}
+          className="w-full object-cover rounded"
+          alt={name}
+        />
       </div>
       <h2 className="text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-headingColor font-[700] mt-3 lg:mt-5">
-        {name}
+        {doctor.name}
       </h2>
       <div className="mt-2 lg:mt-4 flex items-center justify-between">
         <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
-          {specialization}
+          {doctor.specialization}
         </span>
 
         <div className="flex items-center gap-[6px]">
           <span className="flex items-center gap-[6px] text-[14px] leading-6 lg:text-[16px] lg:left-7 font-semibold text-headingColor">
             <img src={starIcon} alt="Star" />
-            {avgRating}
+            {doctor.avgRating || "Not Provided"}
           </span>
           <span className="text-[14px] leading-6 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
-            ({totalRating})
+            ({doctor.totalRating || 0})
           </span>
         </div>
       </div>
@@ -42,10 +47,10 @@ const DoctorCard = ({ doctor }) => {
       <div className="mt-[18px] lg:mt-5 flex items-center justify-between">
         <div>
           <h3 className="text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-headingColor">
-            + {totalPatients} Patients
+            + {totalPatients || 0} Patients
           </h3>
           <p className="text-[14px] leading-6 font-[400] text-textColor">
-            At {hospital}
+            At {hospital || "General Hospital"}
           </p>
         </div>
 
